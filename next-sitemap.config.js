e845f9dbb40a55e6c51517802536e532e3a244cd
Andres Duque <andduque.ing@gmail.com>
2024-04-17 01:59:19 -0500

    Update sitemap with additional sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,13 @@ module.exports = {
   generateRobotsTxt: true, // (optional)
   lastmod: new Date().toISOString(),
   changefreq: 'weekly',
+  additionalSitemaps: [
+    'https://www.andres-duque.com/es',
+    'https://www.andres-duque.com/en',
+    'http://andres-duque.com/',
+    'https://andres-duque.com/',
+    'https://www.andres-duque.com/',
+    'http://www.andres-duque.com/',
+  ],
   // ...other options
 };
