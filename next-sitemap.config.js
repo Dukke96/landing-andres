b708f772b48d3b4372c1f384faeb326ebb28541e
Andres Duque <andduque.ing@gmail.com>
2024-04-17 02:11:31 -0500

    Update sitemap with additional sitemaps and change frequency

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://www.andres-duque.com/',
   generateRobotsTxt: true, // (optional)
   lastmod: new Date().toISOString(),
-  changefreq: 'weekly',
+  changefreq: 'monthly',
   additionalSitemaps: [
     'https://www.andres-duque.com/es',
     'https://www.andres-duque.com/en',
@@ -12,5 +12,28 @@ module.exports = {
     'https://www.andres-duque.com/',
     'http://www.andres-duque.com/',
   ],
+  additionalPaths: async (config) => {
+    const result = [];
+    // all possible values
+    result.push(
+      {
+        loc: '/es',
+        changefreq: 'monthly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/en',
+        changefreq: 'monthly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      }
+    );
+
+    // using transformation from the current configuration
+    result.push(await config.transform(config));
+
+    return result;
+  },
   // ...other options
 };
